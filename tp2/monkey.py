b3236e374e0788558c015eb5d007f530520fb399
geallary <geoffrey.allary.pro@gmail.com>
2020-04-20 13:41:04 +0200

    Ex POO Monkey correction

--- tp2/monkey.py
+++ tp2/monkey.py
@@ -5,6 +5,11 @@ class Monkey:
     def eat(self, banana):
         print(self.name, 'a mangé une banane', banana.color)
 
+    def sex(self, monkey):
+        print(self.name, ' et ', monkey.name, 'ont copulé')
+        robert = Monkey('robert')
+        print(f'{robert} est né')
+        return robert
 
 class Banana:
     def __init__(self, color):
@@ -13,8 +18,14 @@ class Banana:
 
 banana_green = Banana('verte')
 banana_yellow = Banana('jaune')
-pierre = Monkey("Pierre")
-marie = Monkey("bob")
+pierre = Monkey("pierre")
+marie = Monkey("marie")
+robert = Monkey("robert")
 
 pierre.eat(banana_yellow)
 marie.eat(banana_green)
+robert.eat(banana_green)
+
+robert = pierre.sex(marie)
+
+robert.eat(banana_green)
